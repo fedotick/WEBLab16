629a214a89409921d0be75a10501a5015d279fda
fedotick <feodortcaciuc2004@gmail.com>
2024-03-18 13:50:56 +0200

    Task 2 complited

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,40 @@ function encryptData(number) {
 let numToArr = (n) => String(n).split('');
 
 alert(encryptData(1234));
+
+// 2
+document.getElementById('a').addEventListener('input',  triangle);
+document.getElementById('b').addEventListener('input',  triangle);
+document.getElementById('c').addEventListener('input',  triangle);
+
+function triangle() {
+    const a = +document.getElementById('a').value;
+    const b = +document.getElementById('b').value;
+    const c = +document.getElementById('c').value;
+
+    if (!isTriangle(a, b, c)) {
+        document.getElementById('perimeter').innerHTML = "";
+        document.getElementById('area').innerHTML = "";
+        document.getElementById('kind').innerHTML = "";
+        return;
+    }
+    
+    const p = (a + b + c) / 2;
+    let kind = "Произвольный"; 
+
+    if (isEquilateralTriangle(a, b, c)) kind = "Равносторонний"
+    else if (isIsoscelesTriangle(a, b, c) || isIsoscelesTriangle(b, c, a) || isIsoscelesTriangle(c, a, b)) kind = "Равнобедренный";
+
+    document.getElementById('perimeter').innerHTML = a + b + c;
+    document.getElementById('area').innerHTML = Math.sqrt(p * (p - a) * (p - b) * (p - c)).toFixed(2);
+    document.getElementById('kind').innerHTML = kind;
+}
+
+let isTriangle = (a, b, c) => (a + b > c && a + c > b && b + c > a);
+
+let isEquilateralTriangle = (a, b, c) => (a == b && b == c);
+let isIsoscelesTriangle = (a, b, c) => (a == b && a != c);
+
+// let isRightTriangle = (a, b, c) => (a * a + b * b == c * c);
+// let isAcuteTriangle = (a, b, c) => (a * a + b * b > c * c);
+// let isObtuseTriangle = (a, b, c) => (a * a + b * b < c * c);
